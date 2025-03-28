3ca44f4f3df5405723e03e52535c2a9a92e174f3
sephirothbahamut <sephirothbahamut@live.it>
2025-03-28 04:07:17 +0100

    Minor changes

--- Examples/main.cpp
+++ Examples/main.cpp
@@ -32,29 +32,36 @@ int main()
 	utils::MS::graphics::text::format text_format
 		{
 		.font{"Gabriola"},
-		.size{64.f},
-		.alignment{.horizontal{utils::alignment::horizontal::centre}}
+		.size{96.f},
+		.alignment{.horizontal_alignment{utils::alignment::horizontal::centre}}
 		};
 
 	utils::MS::graphics::text::renderer text_renderer{dx_initializer, utils::MS::graphics::text::renderer::create_info{.resolution{resolution}, .clear_colour{background_colour}}};
 	
-	utils::MS::graphics::text::formatted_string formatted_string{"Hello Universe!", text_format, {128.f, 32.f}};
-	
-	formatted_string.properties_regions.rendering.decorators.to_image.add(false, {5, 5});
-	formatted_string.properties_regions.rendering.text.colour      .add(utils::graphics::colour::rgba_f{1.f, 0.f, 0.f, 1.f}, {0, 30});
-	formatted_string.properties_regions.rendering.outline.colour   .add(utils::graphics::colour::rgba_f{.3f, 1.f, .7f, 1.f}, {0, 30});
-	formatted_string.properties_regions.rendering.decorators.colour.add(utils::graphics::colour::rgba_f{1.f, 0.f, 0.f, 1.f}, {0, 11});
-	formatted_string.properties_regions.rendering.decorators.colour.add(utils::graphics::colour::rgba_f{1.f, 1.f, 0.f, 1.f}, {2,  3});
-	formatted_string.properties_regions.rendering.text.to_image    .add(false                                              , {6,  3});
-	formatted_string.properties_regions.rendering.text.colour      .add(utils::graphics::colour::rgba_f{0.f, 1.f, 0.f, 1.f}, {6,  3});
-	formatted_string.properties_regions.rendering.outline.to_image .add(true                                               , {6, 15});
-	formatted_string.properties_regions.formatting.font.add("Arial", {3, 5});
-	formatted_string.properties_regions.formatting.size.add(48.f, {5, 4});
+	utils::MS::graphics::text::formatted_string formatted_string{"Hello Universe!", text_format, {512.f, 256.f}};
+
+	formatted_string.properties_regions.regions_per_field.render.fill         .to_image .add(false, {6,  3});
+	formatted_string.properties_regions.regions_per_field.render.outline      .to_image .add(true , {6, 15});
+	formatted_string.properties_regions.regions_per_field.render.underline    .to_image .add(true , {5,  5});
+	formatted_string.properties_regions.regions_per_field.render.underline    .to_shapes.add(true , {6,  5});
+	formatted_string.properties_regions.regions_per_field.render.strikethrough.to_image .add(true , {5,  5});
+	formatted_string.properties_regions.regions_per_field.render.strikethrough.to_shapes.add(true , {6,  5});
+
+	formatted_string.properties_regions.regions_per_field.render.fill         .colour.add(utils::graphics::colour::rgba_f{1.f, 0.f, 0.f, 1.f}, {0, 30});
+	formatted_string.properties_regions.regions_per_field.render.fill         .colour.add(utils::graphics::colour::rgba_f{0.f, 1.f, 0.f, 1.f}, {6,  3});
+	formatted_string.properties_regions.regions_per_field.render.outline      .colour.add(utils::graphics::colour::rgba_f{.3f, 1.f, .7f, 1.f}, {0, 30});
+	formatted_string.properties_regions.regions_per_field.render.underline    .colour.add(utils::graphics::colour::rgba_f{1.f, 0.f, 0.f, 1.f}, {0, 11});
+	formatted_string.properties_regions.regions_per_field.render.underline    .colour.add(utils::graphics::colour::rgba_f{1.f, 1.f, 0.f, 1.f}, {2,  3});
+	formatted_string.properties_regions.regions_per_field.render.strikethrough.colour.add(utils::graphics::colour::rgba_f{1.f, 0.f, 0.f, 1.f}, {0, 11});
+	formatted_string.properties_regions.regions_per_field.render.strikethrough.colour.add(utils::graphics::colour::rgba_f{1.f, 1.f, 0.f, 1.f}, {2,  3});
+	formatted_string.properties_regions.regions_per_field.format.font.add("Arial", {3, 5});
+	formatted_string.properties_regions.regions_per_field.format.size.add(48.f, {5, 4});
+	formatted_string.properties_regions.regions_per_field.format.strikethrough.add(true, {5, 5});
 	const auto renderable{formatted_string.shrink_to_fit(dx_initializer)};
 
 	text_renderer.draw_text(renderable, {0.f, 0.f});
 
-	const auto image{text_renderer.get_output().image};
+	const auto image{text_renderer.get_output_image()};
 
 	std::filesystem::create_directories("./output");
 	utils::graphics::image::save_to_file(image, "output/test.png");
--- Examples/utils_implementation.cpp
+++ Examples/utils_implementation.cpp
@@ -9,4 +9,3 @@
 
 #include <utils/MS/raw/graphics/text/custom_renderer.h>
 #include <utils/MS/graphics/text/formatted_string.h>
-#include <utils/MS/graphics/text/properties.h>
